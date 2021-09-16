b763a9c391f028a4f1e645a1b0e334790c5090e5
yugaro <yugaro.yugaro@gmail.com>
2021-09-16 20:08:27 +0900

    Initial Commit

--- src/plot_data.py
+++ src/plot_data.py
@@ -1,13 +1,13 @@
 from blueprint.set_args import set_args
 from model.vehicle import Vehicle
-from view.view import plot_traj_trigger
-from view.view import plot_contractive_set
-from view.view import plot_traj_safe
-from view.view import plot_u_data
-from view.view import plot_horizon
-from view.view import plot_jcost
-from view.view import plt_traj_all
-from view.view import plt_traj_xe
+# from view.view import plot_traj_trigger
+# from view.view import plot_contractive_set
+# from view.view import plot_traj_safe
+# from view.view import plot_u_data
+# from view.view import plot_horizon
+# from view.view import plot_jcost
+# from view.view import plt_traj_all
+# from view.view import plt_traj_xe
 from view.view import plot_3d
 
 
